f820225245e3732e4182c538ad90f87ad8f11071
Iman <iman.mansour94@gmail.com>
2018-04-19 18:11:43 +0200

    add resolve ticket

--- ticket_system/urls.py
+++ ticket_system/urls.py
@@ -4,5 +4,5 @@ import views
 urlpatterns = [
     url(r'^all/$', views.all_tickets_view),
     url(r'^add/$', views.add_ticket_view),
-    url(r'^edit/(?P<ticket_id>[0-9]+)/$', views.edit_ticket_view),
+    url(r'^edit/(?P<ticket_id>[0-9]+)/$', views.change_status_view),
 ]
--- ticket_system/views.py
+++ ticket_system/views.py
@@ -1,44 +1,49 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, HttpResponse
 from ticket_system.models import Ticket
 from ticket_system.forms import TicketForm
 
 
 def all_tickets_view(request):
-   all_tickets = Ticket.objects.order_by('ticket_status')
+   all_tickets = Ticket.objects.order_by('ticket_status');
    context = {
        "tickets":all_tickets
    }
-   return render(request,'all_tickets.html',context)
+   return render(request,'all_tickets.html',context);
 
 ################################################
 
 def add_ticket_view(request):
 
-    form = TicketForm()
+    form = TicketForm();
     if request.method == "POST":
-        form = TicketForm(request.POST)
+        form = TicketForm(request.POST);
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/tickets/all")
+            form.save();
+            return HttpResponseRedirect("/tickets/all");
     context = {
         "form":form,
     }
-    return render(request,"ticket_form.html",context)
+
+    return render(request,"ticket_form.html",context);
 
 ##################################################
 
-def edit_ticket_view(request, ticket_id):
-    obj = Ticket.objects.get(id = ticket_id)
-    form = TicketForm(instance=obj)
+def change_status_view(request, ticket_id):
 
-    if request.method == "POST":
-        form = TicketForm(request.POST, instance=obj)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/tickets/all')
 
-    context = {
-        "form":form
-    }
-    return render(request,'ticket_form.html',context)
+    ticket = Ticket.objects.get(id = ticket_id);
+
+    ####Toggle Status######
+
+    ticket.ticket_status = 0 if ticket.ticket_status else 1;
+
+    ticket.save();
+
+    ### Send Email to notify user that case is resolved ######
+
+    
+
+
+
+    return HttpResponseRedirect("/tickets/all");
