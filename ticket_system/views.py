d21d53eff752b2973db14b00028c7c451a863757
Iman <iman.mansour94@gmail.com>
2018-04-19 16:37:18 +0200

    add views

--- ticket_system/views.py
+++ ticket_system/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, Http404
+from ticket_system.models import Ticket
+from ticket_system.forms import TicketForm
 
-# Create your views here.
+
+def all_tickets_view(request):
+   all_tickets = Ticket.objects.all()
+   context = {
+       "tickets":all_tickets
+   }
+   return render(request,'alltickets.html',context)
+
+################################################
+
+def add_ticket_view(request):
+
+    form = TicketForm()
+    if request.method == "POST":
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/tickets/all")
+    context = {
+        "form":form,
+    }
+    return render(request,"ticket_form.html",context)
+
+##################################################
+
+def edit_ticket_view(request, ticket_id):
+    obj = Ticket.objects.get(id = ticket_id)
+    form = TicketForm(instance=obj)
+
+    if request.method == "POST":
+        form = TicketForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/tickets/all')
+
+    context = {
+        "form":form
+    }
+    return render(request,'ticket_form.html',context)
