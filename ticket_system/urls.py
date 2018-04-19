fa88c76583ed57fe0342960eb81479ebf650f740
Iman <iman.mansour94@gmail.com>
2018-04-19 16:24:59 +0200

    add urls

---
+++ ticket_system/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+import views
+
+urlpatterns = [
+    url(r'^all/$', views.all_tickets_view),
+    url(r'^add/$', views.add_ticket_view),
+    url(r'^edit/(?P<ticket_id>[0-9]+)/$', views.edit_ticket_view),
+]
