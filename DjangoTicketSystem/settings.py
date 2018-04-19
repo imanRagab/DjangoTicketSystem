f2efd16f643920a1d4a1f53b0092ed67f2fb5f25
Iman <iman.mansour94@gmail.com>
2018-04-19 15:30:39 +0200

    create app

--- DjangoTicketSystem/settings.py
+++ DjangoTicketSystem/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'DjangoTicketSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoTicketSystem', #databse name
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost', #default host
+        'PORT': '3306' #default port
     }
 }
 
